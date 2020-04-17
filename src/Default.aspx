453725448474509b05527acfd1a00eb26bd8e551
Wanderson Caldas <wcaldasti@gmail.com>
2020-04-17 10:38:28 -0300

    CRIAR PROJETO

---
+++ src/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src='https://www.google.com/recaptcha/api.js' type="text/javascript"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table cellspacing="0" style="width:100%;" id="tb_captcha" runat="server"> 
+                <tr>
+                    <td>
+                        <div class="g-recaptcha" data-sitekey="CHAVE_SITE"></div>                
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="btnIncluir" runat="server" Text="TESTAR" ClientIDMode="Static" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ src/Default.aspx.vb
@@ -0,0 +1,57 @@
+﻿
+Imports System.IO
+Imports System.Net
+Imports System.Web.Script.Serialization
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+
+        End If
+    End Sub
+
+
+    Protected Sub btnIncluir_Click(sender As Object, e As EventArgs) Handles btnIncluir.Click
+        If (capValidate()) Then
+            ClientScript.RegisterStartupScript(Me.GetType, "erro", "<script language='javascript'>alert('SUCESSO');</script>")
+        Else
+            ClientScript.RegisterStartupScript(Me.GetType, "erro", "<script language='javascript'>alert('FALHA');</script>")
+        End If
+    End Sub
+
+    Public Function capValidate() As Boolean
+        Dim Response As String = Request("g-recaptcha-response")
+        Dim Valid As Boolean = False
+        Dim req As HttpWebRequest = DirectCast(WebRequest.Create(Convert.ToString("https://www.google.com/recaptcha/api/siteverify?secret=CHAVE_SECRETA&response=") & Response), HttpWebRequest)
+        Try
+            Using wResponse As WebResponse = req.GetResponse()
+
+                Using readStream As New StreamReader(wResponse.GetResponseStream())
+                    Dim jsonResponse As String = readStream.ReadToEnd()
+                    Dim js As New JavaScriptSerializer()
+                    Dim data As MyObject = js.Deserialize(Of MyObject)(jsonResponse)
+
+                    Valid = Convert.ToBoolean(data.success)
+                    Return Valid
+                End Using
+            End Using
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+End Class
+
+Public Class MyObject
+    Public Property success() As String
+        Get
+            Return m_success
+        End Get
+        Set(value As String)
+            m_success = value
+        End Set
+    End Property
+    Private m_success As String
+
+End Class
